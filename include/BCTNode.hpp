f0a352e5271b64c7986a6303925268a5206cbf23
MNS-MSI <sspark@kmou.ac.kr>
2019-05-11 17:49:50 +0900

    [WIP] BCTNode

--- include/BCTNode.hpp
+++ include/BCTNode.hpp
@@ -1,4 +1,39 @@
 #ifndef BCT_NODE_HPP_
 #define BCT_NODE_HPP_
 
+#include <forward_list>
+#include <string>
+#include <unordered_set>
+
+class BCTForest;
+
+class BCTNode {
+ public:
+  explicit BCTNode(const std::string &source_node,
+                   const std::string &target_node);
+  ~BCTNode();
+
+  bool contains_node(const std::string &name);
+
+  // static BCTForest &Forest() { return forest_; }
+
+ private:
+  static BCTForest forest_;
+
+  std::unordered_set<std::string>
+      name_content_;  // memory overhead and performance warning
+  double cost_;
+  BCTNode *parent_, *left_, *right_;
+};
+
+class BCTForest {
+ public:
+  BCTNode *find_root_contains_node(const std::string &name);
+  void erase_root(BCTNode *root);
+  void insert_root(BCTNode *root);
+
+ private:
+  std::forward_list<BCTNode *> trees;
+};
+
 #endif  // BCT_NODE_HPP_
--- src/BCTNode.cpp
+++ src/BCTNode.cpp
@@ -1,80 +1,91 @@
 #include <forward_list>
-#include <iostream>
 #include <string>
 #include <unordered_set>
 
 #include "BCTNode.hpp"
 
-class BCTNode {
- public:
-  explicit BCTNode(const std::string &source_node,
-                   const std::string &target_node)
-      : parent_(nullptr) {
-    BCTNode *source_root = forest_.find_root_contains_node(source_node);
-    BCTNode *target_root = forest_.find_root_contains_node(target_node);
+BCTNode::BCTNode(const std::string &source_node, const std::string &target_node)
+    : parent_(nullptr) {
+  BCTNode *source_root = forest_.find_root_contains_node(source_node);
+  BCTNode *target_root = forest_.find_root_contains_node(target_node);
 
-    if (source_root && target_root) {
-      left_ = source_root;
-      right_ = target_root;
+  if (source_root && target_root) {
+    left_ = source_root;
+    right_ = target_root;
 
-      left_->parent_ = this;
-      right_->parent_ = this;
+    name_content_ = source_root->name_content_;
+    name_content_.insert(target_root->name_content_.begin(),
+                         target_root->name_content_.end());
 
-      forest_.erase_root(source_root);
-      forest_.erase_root(target_root);
-      forest_.insert_root(this);
-    } else if (source_root) {
-    } else if (target_root) {
+    source_root->parent_ = this;
+    target_root->parent_ = this;
+
+    forest_.erase_root(source_root);
+    forest_.erase_root(target_root);
+    forest_.insert_root(this);
+  } else if (source_root) {
+    left_ = source_root;
+    right_ = nullptr;
+
+    name_content_ = source_root->name_content_;
+    name_content_.insert(target_node);
+
+    source_root->parent_ = this;
+
+    forest_.erase_root(source_root);
+    forest_.insert_root(this);
+  } else if (target_root) {
+    left_ = nullptr;
+    right_ = target_root;
+
+    name_content_ = target_root->name_content_;
+    name_content_.insert(source_node);
+
+    target_root->parent_ = this;
+
+    forest_.erase_root(target_root);
+    forest_.insert_root(this);
+  } else {
+    left_ = nullptr;
+    right_ = nullptr;
+
+    name_content_.insert(source_node);
+    name_content_.insert(target_node);
+
+    forest_.insert_root(this);
+  }
+}
+
+BCTNode::~BCTNode() {}
+
+bool BCTNode::contains_node(const std::string &name) {
+  return name_content_.find(name) != name_content_.end();
+}
+
+
+BCTNode *BCTForest::find_root_contains_node(const std::string &name) {
+  BCTNode *root = nullptr;
+  for (auto itr = trees.begin(); itr != trees.end(); ++itr) {
+    if ((*itr)->contains_node(name)) {
+      root = *itr;
     }
   }
-  ~BCTNode() {}
 
-  bool contains_node(const std::string &name) {
-    return name_content_.find(name) != name_content_.end();
+  return root;
+}
+
+void BCTForest::erase_root(BCTNode *root) {
+  if (!root) {
+    return;
   }
 
- private:
-  // BCTNode *merge(BCTNode *node) {}
-
-  static BCTForest forest_;
-
-  std::unordered_set<std::string>
-      name_content_;  // memory overhead and performance warning
-  double cost_;
-  BCTNode *parent_, *left_, *right_;
-};
-
-class BCTForest {
- public:
-  BCTNode *find_root_contains_node(const std::string &name) {
-    BCTNode *root = nullptr;
-    for (auto itr = trees.begin(); itr != trees.end(); ++itr) {
-      if ((*itr)->contains_node(name)) {
-        root = *itr;
-      }
-    }
-
-    return root;
-  }
-
-  void erase_root(BCTNode *root) {
-    if (!root) {
-      return;
-    }
-
-    for (auto itr = trees.begin(); itr != trees.end(); ++itr) {
-      if ((*itr) == root) {
-        auto itr_before = std::prev(itr, 1);
-        trees.erase_after(itr_before);
-        break;
-      }
+  for (auto itr = trees.begin(); itr != trees.end(); ++itr) {
+    if ((*itr) == root) {
+      auto itr_before = std::prev(itr, 1);
+      trees.erase_after(itr_before);
+      break;
     }
   }
+}
 
-  void insert_root(BCTNode *root) {
-    trees.emplace_front(root);
-  }
-
- private:
-  std::forward_list<BCTNode *> trees;
-};
+void BCTForest::insert_root(BCTNode *root) { trees.emplace_front(root); }
